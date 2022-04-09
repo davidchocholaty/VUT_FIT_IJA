de46ee9636318f2767410dd036ff3b9ef456390c
davidchocholaty <david.chocholaty@seznam.cz>
2022-04-09 20:23:03 +0200

    Add basic structure of class tag parsing

--- src/main/java/com/uml/filehandler/IJAXMLParser.java
+++ src/main/java/com/uml/filehandler/IJAXMLParser.java
@@ -1,8 +1,6 @@
 package com.uml.filehandler;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -12,11 +10,16 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
+/* https://mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/ */
 public class IJAXMLParser {
     private final String filePath;
+    private Document doc;
+    private int firstLevelOrder;
 
     public IJAXMLParser(String filePath) {
         this.filePath = filePath;
+        this.doc = null;
+        this.firstLevelOrder = 0;
     }
 
     public void parse() {
@@ -26,30 +29,121 @@ public class IJAXMLParser {
             dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 
             DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new File(this.filePath));
+            this.doc = db.parse(new File(this.filePath));
 
             // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-            doc.getDocumentElement().normalize();
+            this.doc.getDocumentElement().normalize();
 
-            NodeList list;
-
-            /* Iterate through classes */
-            list = doc.getElementsByTagName("class");
-
-            for (int i = 0; i < list.getLength(); i++) {
-
+            if (this.doc.hasChildNodes()) {
+                parseClasses();
+                parseRelationships();
             }
-
-            /* Iterate through relationships */
-            list = doc.getElementsByTagName("relationship");
-
-            for (int i = 0; i < list.getLength(); i++) {
-
-            }
-
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void parseClasses() {
+        /* Iterate through classes */
+        NodeList list = this.doc.getChildNodes();
+        Node node;
+
+        while (this.firstLevelOrder < list.getLength()) {
+            node = list.item(this.firstLevelOrder);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                if (!node.getNodeName().equals("class")) {
+                    break;
+                }
+
+                /* TODO class tag */
+
+                if (node.hasAttributes()) {
+                    NamedNodeMap nodeMap = node.getAttributes();
+
+                    if (nodeMap.getLength() != 1 || !node.getNodeName().equals("name")) {
+                        // TODO error
+                    } else {
+                        /* TODO class tag attribute name */
+                    }
+                }
+
+                if (node.hasChildNodes()) {
+                    parseClassChildren(node);
+                }
+            }
+
+            this.firstLevelOrder++;
+        }
+    }
+
+    private void parseClassChildren(Node classNode) {
+        NodeList list = classNode.getChildNodes();
+        Node node;
+        int idx = 0;
+
+        /* Abstract tag */
+        node = list.item(idx);
+        idx++;
+
+        if (!node.getNodeName().equals("abstract")) {
+            // TODO error
+        } else {
+            /* TODO abstract tag */
+        }
+
+        /* xCoordinate tag */
+        node = list.item(idx);
+        idx++;
+
+        if (!node.getNodeName().equals("xCoordinate")) {
+            // TODO error
+        } else {
+            /* TODO xCoordinate tag */
+        }
+
+        /* yCoordinate tag */
+        node = list.item(idx);
+        idx++;
+
+        if (!node.getNodeName().equals("yCoordinate")) {
+            // TODO error
+        } else {
+            /* TODO yCoordinate tag */
+        }
+
+        /* visibility tag */
+        node = list.item(idx);
+        idx++;
+
+        if (!node.getNodeName().equals("visibility")) {
+            // TODO error
+        } else {
+            /* TODO visibility tag */
+        }
+
+        /* attributes tags */
+        parseAttributes(classNode, idx);
+
+        /* operations tags */
+        parseOperations(classNode, idx);
+    }
+
+    private void parseAttributes(Node parentNode, int idx) {
+
+    }
+
+    private void parseOperations(Node classNode, int idx) {
+
+    }
+
+    private void parseRelationships() {
+        /* Iterate through relationships */
+        NodeList relList = this.doc.getElementsByTagName("relationship");
+
+        for (int i = 0; i < relList.getLength(); i++) {
+
         }
     }
 }
