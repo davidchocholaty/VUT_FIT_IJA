a512cb7c642d72359535d32a0a62968a6687659f
adamkankovsky <adamkankovsky@gmail.com>
2022-04-11 13:46:58 +0200

    :bug: - fix bugs

--- src/main/java/com/uml/ClassUML.java
+++ src/main/java/com/uml/ClassUML.java
@@ -14,7 +14,7 @@ import com.uml.classdiagram.UMLClass;
 public class ClassUML extends Button{
     public Node view;
     public ObservableList<Arrow> edges = FXCollections.observableArrayList();
-    private ClassController controller;
+    private final ClassController controller;
 
     public ClassUML(double x, double y, MainController controller) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource("class_uml.fxml"));
@@ -25,6 +25,8 @@ public class ClassUML extends Button{
         getView().translateXProperty().bind(loaded.widthProperty().divide(-2));
         getView().translateYProperty().bind(loaded.heightProperty().divide(-2));
 
+        this.controller = loader.getController();
+        this.controller.setParentController(controller);
 
         UMLClass cls = controller.diagram.createDefaultClass();
         cls.setXCoordinate(x);
@@ -44,6 +46,7 @@ public class ClassUML extends Button{
         getView().translateYProperty().bind(loaded.heightProperty().divide(-2));
 
         this.controller = loader.getController();
+        this.controller.setParentController(controller);
 
         controller.diagram.createClass(name);
 
