f9eaecc83765f12d4aac095f37311e0c7177fafa
davidchocholaty <david.chocholaty@seznam.cz>
2022-05-03 13:27:50 +0200

    Add documentation comments to custom exceptions

--- src/main/java/com/uml/customexception/IllegalFileExtension.java
+++ src/main/java/com/uml/customexception/IllegalFileExtension.java
@@ -1,5 +1,18 @@
+/**
+ * Project for course IJA at FIT BUT.
+ * <p>
+ *     Enum representing UML visibility types.
+ * </p>
+ * @author: David Chocholaty <xchoch09@stud.fit.vutbr.cz>
+ * @author: Adam Kankovsky <xkanko00@stud.fit.vutbr.cz>
+ */
+
+
 package com.uml.customexception;
 
+/**
+ * The class representing illegal file extension custom exception.
+ */
 public class IllegalFileExtension extends Exception {
     /**
      * Creates a new instance of IllegalFileExtension.
--- src/main/java/com/uml/customexception/IllegalFileFormat.java
+++ src/main/java/com/uml/customexception/IllegalFileFormat.java
@@ -1,3 +1,13 @@
+/**
+ * Project for course IJA at FIT BUT.
+ * <p>
+ *     Enum representing UML visibility types.
+ * </p>
+ * @author: David Chocholaty <xchoch09@stud.fit.vutbr.cz>
+ * @author: Adam Kankovsky <xkanko00@stud.fit.vutbr.cz>
+ */
+
+
 package com.uml.customexception;
 
 /**
